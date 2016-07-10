19ac7a50c21fc8aeedc4373c91f0f405bc7280d8
Vic <1006510007@qq.com>
2016-07-10 17:36:54 +0800

    Test CheckBox

--- Simpleview/src/main/java/com/example/simpleview/MainActivity.java
+++ Simpleview/src/main/java/com/example/simpleview/MainActivity.java
@@ -4,22 +4,36 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.ArrayAdapter;
 import android.widget.AutoCompleteTextView;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
 import android.widget.MultiAutoCompleteTextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     private AutoCompleteTextView completeTextView;
     private MultiAutoCompleteTextView multiAutoCompleteTextView;
     private String[] res = {"hello", "hello1", "hello2", "world", "world1"};
+    private CheckBox checkBox;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         completeTextView = (AutoCompleteTextView) findViewById(R.id.AcTv);
-        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this,
                 android.R.layout.simple_list_item_1, res);
         completeTextView.setAdapter(adapter);
         multiAutoCompleteTextView = (MultiAutoCompleteTextView) findViewById(R.id.MacTv);
         multiAutoCompleteTextView.setAdapter(adapter);
         multiAutoCompleteTextView.setTokenizer(new MultiAutoCompleteTextView.CommaTokenizer());
+        checkBox = (CheckBox) findViewById(R.id.ckb);
+        checkBox.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
+                Toast.makeText(MainActivity.this, "Changed", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
+
+
 }
