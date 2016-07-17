e50b4a01fd43060cc4564c17a141d0d246ffe0a5
Vic <1006510007@qq.com>
2016-07-17 23:58:16 +0800

    Test SharedPreferences.

--- SharedPreferences/src/main/java/com/example/sharedpreferences/MainActivity.java
+++ SharedPreferences/src/main/java/com/example/sharedpreferences/MainActivity.java
@@ -6,9 +6,16 @@ import android.preference.PreferenceManager;
 import android.provider.Settings;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-
+    EditText UserName,UserPass;
+    CheckBox chk;
+    SharedPreferences pref;
+    SharedPreferences.Editor edtior;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -16,16 +23,53 @@ public class MainActivity extends AppCompatActivity {
 //        SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences
 //                (MainActivity.this
 //        );
-        SharedPreferences sharedPreferences = getSharedPreferences("myPre", MODE_PRIVATE);
-        SharedPreferences.Editor edit = sharedPreferences.edit();
-        edit.putString("name", "Vic");
-        edit.putInt("age", 22);
-        edit.putLong("time", System.currentTimeMillis());
-        edit.putBoolean("default", true);
-        edit.commit();
-        edit.remove("default");
-        edit.commit();
-        System.out.println(sharedPreferences.getString("name", ""));
-        System.out.println(sharedPreferences.getInt("age", 0));
+//        SharedPreferences sharedPreferences = getSharedPreferences("myPre", MODE_PRIVATE);
+//        SharedPreferences.Editor edit = sharedPreferences.edit();
+//        edit.putString("name", "Vic");
+//        edit.putInt("age", 22);
+//        edit.putLong("time", System.currentTimeMillis());
+//        edit.putBoolean("default", true);
+//        edit.commit();
+//        edit.remove("default");
+//        edit.commit();
+//        System.out.println(sharedPreferences.getString("name", ""));
+//        System.out.println(sharedPreferences.getInt("age", 0));
+        UserName = (EditText) findViewById(R.id.userName);
+        UserPass = (EditText) findViewById(R.id.userPasswd);
+        chk = (CheckBox) findViewById(R.id.chkSaveName);
+        pref = getSharedPreferences("UserInfo", MODE_PRIVATE);
+        edtior = pref.edit();
+        String name = pref.getString("userName", "");
+        if (name == null) {
+            chk.setChecked(false);
+        } else {
+            chk.setChecked(true);
+            UserName.setText(name);
+        }
+    }
+
+    public void doClick(View v) {
+        switch (v.getId()) {
+            case R.id.btnLogin:
+                String name = UserName.getText().toString().trim();
+                String pass = UserPass.getText().toString().trim();
+                if ("admin".equals(name) && "123456".equals(pass)) {
+                    if (chk.isChecked()) {
+                        edtior.putString("userName", name);
+                        edtior.commit();
+
+                    } else {
+                        edtior.remove("userName");
+                        edtior.commit();
+                    }
+                    Toast.makeText(MainActivity.this, "登陆成功", Toast.LENGTH_LONG).show();
+                } else {
+                    Toast.makeText(MainActivity.this, "禁止登陆", Toast.LENGTH_LONG).show();
+                }
+                break;
+
+            default:
+                break;
+        }
     }
 }
