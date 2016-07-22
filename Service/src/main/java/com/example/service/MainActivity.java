5632945969746d3b9c4be12759f85cc85969ea6f
Vic <1006510007@qq.com>
2016-07-23 00:16:33 +0800

    Test Service.

--- Service/src/main/java/com/example/service/MainActivity.java
+++ Service/src/main/java/com/example/service/MainActivity.java
@@ -1,8 +1,10 @@
 package com.example.service;
 
 import android.app.Service;
+import android.content.ComponentName;
 import android.content.Intent;
 import android.content.ServiceConnection;
+import android.os.IBinder;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -12,6 +14,8 @@ import android.widget.Button;
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
     Intent intent;
     Intent intentBind;
+    MyBindService bindService;
+
     private Button buttonStart;
     private Button buttonStop;
     private Button buttonBind;
@@ -20,6 +24,22 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     private Button buttonNext;
     private Button buttonPre;
     private Button buttonUnbind;
+
+
+    ServiceConnection connection = new ServiceConnection() {
+        @Override
+        //当启动源和Service成功连接之后会自动调用这个方法
+        public void onServiceConnected(ComponentName componentName, IBinder iBinder) {
+            bindService = ((MyBindService.MyBinder) iBinder).getService();
+        }
+
+        @Override
+        //当启动源和Service的连接意外丢失的时候会调用这个方法
+        //比如Service崩溃了或者被强行杀死了
+        public void onServiceDisconnected(ComponentName componentName) {
+
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,6 +54,16 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         buttonNext = (Button) findViewById(R.id.next);
         buttonPre = (Button) findViewById(R.id.pre);
         buttonUnbind = (Button) findViewById(R.id.unbind);
+
+        buttonStart.setOnClickListener(this);
+        buttonStop.setOnClickListener(this);
+        buttonBind.setOnClickListener(this);
+        buttonPlay.setOnClickListener(this);
+        buttonPause.setOnClickListener(this);
+        buttonNext.setOnClickListener(this);
+        buttonPre.setOnClickListener(this);
+        buttonUnbind.setOnClickListener(this);
+
     }
 
     @Override
@@ -48,19 +78,31 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 break;
             case R.id.bind:
                 intentBind = new Intent(MainActivity.this, MyBindService.class);
-                bindService(intentBind, null, Service.BIND_AUTO_CREATE);
+                bindService(intentBind, connection, Service.BIND_AUTO_CREATE);
                 break;
             case R.id.play:
+                bindService.play();
                 break;
             case R.id.pause:
+                bindService.pause();
                 break;
             case R.id.next:
+                bindService.next();
                 break;
             case R.id.pre:
+                bindService.pre();
                 break;
             case R.id.unbind:
-                unbindService(null);
+                unbindService(connection);
                 break;
         }
+    }
+
+
+    @Override
+    protected void onDestroy() {
+        stopService(intentBind);
+        unbindService(connection);
+        super.onDestroy();
     }
 }
--- Service/src/main/java/com/example/service/MyBindService.java
+++ Service/src/main/java/com/example/service/MyBindService.java
@@ -1,38 +1,65 @@
 package com.example.service;
 
+import android.Manifest;
 import android.app.Service;
 import android.content.Intent;
 import android.content.ServiceConnection;
+import android.os.Binder;
 import android.os.IBinder;
 import android.support.annotation.Nullable;
 import android.util.Log;
+import android.widget.Toast;
 
 /**
  * Created by Vic on 2016/7/22.
  */
 public class MyBindService extends Service {
-    @Nullable
+
+
+    public class MyBinder extends Binder {
+        public MyBindService getService() {
+            return MyBindService.this;
+        }
+    }
+
     @Override
     public IBinder onBind(Intent intent) {
-        Log.d("TAG","BindService onBind");
-        return null;
+        Log.d("TAG", "BindService onBind");
+        return new MyBinder();
     }
 
     @Override
     public void onCreate() {
-        Log.d("TAG","BindService onCreate");
+        Log.d("TAG", "BindService onCreate");
         super.onCreate();
     }
 
     @Override
     public void unbindService(ServiceConnection conn) {
-        Log.d("TAG","BindService unbindService");
+        Log.d("TAG", "BindService unbindService");
         super.unbindService(conn);
     }
 
     @Override
     public void onDestroy() {
-        Log.d("TAG","BindService onDestroy");
+        Log.d("TAG", "BindService onDestroy");
         super.onDestroy();
+    }
+
+
+    public void play() {
+        Toast.makeText(this, "Play!", Toast.LENGTH_SHORT).show();
+    }
+
+    public void pause() {
+        Toast.makeText(this, "Pause!", Toast.LENGTH_SHORT).show();
+    }
+
+    public void next() {
+        Toast.makeText(this, "Next!", Toast.LENGTH_SHORT).show();
+    }
+
+    public void pre() {
+        Toast.makeText(this, "pre!", Toast.LENGTH_SHORT).show();
     }
 }
